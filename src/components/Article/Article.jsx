07357b724e4c58f0496e769c70f429c62e20c4c1
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-04-17 14:56:33 -0300

    refact: update to functional components

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Article.css";
 
-export class Article extends Component {
-  render() {
-    return (
-      <article>
-        <img
-          src={this.props.thumbnail}
-          alt={this.props.description}
-          width={350}
-        />
-        <div className="article-infos">
-          <h2>{this.props.title}</h2>
-          <h3>{this.props.provider}</h3>
-          <p>{this.props.description}</p>
-        </div>
-      </article>
-    );
-  }
-}
+export const Article = ({thumbnail, description, title, provider}) => {
+  return (
+    <article>
+      <img
+        src={thumbnail}
+        alt={description}
+        width={350}
+      />
+      <div className="article-infos">
+        <h2>{title}</h2>
+        <h3>{provider}</h3>
+        <p>{description}</p>
+      </div>
+    </article>
+  );
+};
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,31 +3,29 @@ import logo from "../../assets/images/logo.svg";
 
 import "./NavBar.css";
 
-export class NavBar extends React.Component {
-  render() {
-    return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src={logo} alt="Logo da Space Flight News" />
-            <h1>Space Flight News</h1>
-          </div>
-          <ul className="nav-list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Trending</a>
-            </li>
-            <li>
-              <a href="#">Categories</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+export const NavBar = () => {
+  return (
+    <header>
+      <nav id="navbar">
+        <div className="nav-brand">
+          <img src={logo} alt="Logo da Space Flight News" />
+          <h1>Space Flight News</h1>
+        </div>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
