0df332804b95ea9dc641f017e7213bfef280e788
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-04-17 11:33:38 -0300

    feat: create navbar component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/App.css";
+import NavBar from "./components/NavBar/NavBar";
+
+class App extends React.Component {
+  render() {
+    return <NavBar />;
+  }
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../../assets/images/logo.svg";
+
+import './NavBar.css'
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src={logo} alt="Logo da Space Flight News" />
+            <h1>Space Flight News</h1>
+          </div>
+          <ul className="nav-list">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Trending</a>
+            </li>
+            <li>
+              <a href="#">Categories</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
