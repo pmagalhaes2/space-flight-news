4b785426ce6e64618af6e38a0f5ca50c8a3980a8
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-04-17 21:19:07 -0300

    chore: load news dynamically and create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Article } from "./components/Article/Article";
+import axios from "axios";
+import { Footer } from "./components/Footer/Footer";
 
-function App () {
+function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v4/articles"
+      );
+      const { results } = response.data;
+
+      setNews(results);
+    }
+    loadNews();
+  }, []);
+
   return (
     <>
       <NavBar />
       <section id="articles">
-        <Article
-          thumbnail="https://raw.githubusercontent.com/TheSpaceDevs/spaceflightnewsapi/main/.github/profile/assets/snapi_poster.png"
-          title="Designing Dashboards"
-          provider="NASA"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            consequuntur exercitationem sit!"
-        />
-        <Article
-          thumbnail="https://raw.githubusercontent.com/TheSpaceDevs/spaceflightnewsapi/main/.github/profile/assets/snapi_poster.png"
-          title="Vibrant Portraits of 2020"
-          provider="SpaceNews"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            consequuntur exercitationem sit!"
-        />
-        <Article
-          thumbnail="https://raw.githubusercontent.com/TheSpaceDevs/spaceflightnewsapi/main/.github/profile/assets/snapi_poster.png"
-          title="36 Days of Malayalam type"
-          provider="Spaceflight Now"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            consequuntur exercitationem sit!"
-        />
+        {news.map((article) => (
+          <Article
+            key={article.id}
+            articleUrl={article.url}
+            title={article.title}
+            thumbnail={article.image_url}
+            provider={article.news_site}
+            description={article.summary}
+          />
+        ))}
       </section>
+      <Footer />
     </>
   );
-};
+}
 
 export default App;
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,16 +2,22 @@ import React from "react";
 
 import "./Article.css";
 
-export const Article = ({thumbnail, description, title, provider}) => {
+export const Article = ({
+  articleUrl,
+  thumbnail,
+  description,
+  title,
+  provider,
+}) => {
   return (
     <article>
-      <img
-        src={thumbnail}
-        alt={description}
-        width={350}
-      />
+      <img src={thumbnail} alt={description} />
       <div className="article-infos">
-        <h2>{title}</h2>
+        <h2>
+          <a href={articleUrl} target="_blank" rel="noreferrer">
+            {title}
+          </a>
+        </h2>
         <h3>{provider}</h3>
         <p>{description}</p>
       </div>
